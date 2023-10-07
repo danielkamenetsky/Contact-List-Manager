5be275026da0983c55181625aa99b3a03e2c4717
Daniel Kamenetsky <20dk19@queensu.ca>
2023-10-07 19:00:06 -0400

    Initial commit for Spring Initializr set up

---
+++ src/main/java/com/clm/contactlistmanager/ContactListManagerApplication.java
@@ -0,0 +1,13 @@
+package com.clm.contactlistmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactListManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactListManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clm/contactlistmanager/ContactListManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clm.contactlistmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactListManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
